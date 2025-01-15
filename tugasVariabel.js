34112cc1736e6115b7d4ee48f4d3a948d8abd179
kayla135 <zkayla561@gmail.com>
2025-01-15 11:15:51 +0700

    Add files via upload

---
+++ tugasVariabel.js
@@ -0,0 +1,16 @@
+let nama = "Kayla zahra azalia m.";
+let alamat = "pilangsari";
+let usia = "15";
+let ttl = "Demak, 26 April 2009";
+let gender = "perempuan";
+let agama = "islam";
+let email = "zkayla561@gmail.com";
+let berat = "45kg";
+let tinggi = "155cm";
+let hobi = "rebahan";
+let cita = "pramugari";
+let pekerjaan = "pelajar";
+let sekolah = "SMKN 1 Sayung";
+let jurusan = "PPLG";
+
+document.write("nama saya adalah "+ nama+ ", saya tinggal di "+ alamat+ " , pada tahun 2024 ini usia saya adalah "+ usia+" tahun, saya lahir di "+ ttl+", jeis kelamin saya adalah "+ gender+", saya beragama "+ agama+" , Alamat email saya adalah "+ email+" , saya memiliki berat "+ berat+" , dengan tinggi "+ tinggi+" , saya sangat senang "+ hobi+", karena saya memiliki cita cita sebagai "+ cita+" , saat ini saya adalah seorang "+ pekerjaan+" yang bersekolah di "+ sekolah+" pada jurusan "+ jurusan+"" )
